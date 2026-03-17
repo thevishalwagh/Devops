be70249c53bc9486dcc8178016e32c44d1a5b804
thevishalwagh <wagh.vishal.edu@gmail.com>
2026-03-18 04:48:06 +0530

    Major Changes

--- projects/my-jenkins-app/src/main/java/com/example/HelloController.java
+++ projects/my-jenkins-app/src/main/java/com/example/HelloController.java
@@ -1,3 +1,7 @@
+package com.example;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HelloController {
 
